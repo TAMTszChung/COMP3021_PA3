a03aa2cd47d6350b6a33856454e58d3d8e9fc541
TAMTszChung <tctam@connect.ust.hk>
2020-12-08 19:41:55 +0800

    Implement MakeMoveByBehavior

--- src/main/java/castle/comp3021/assignment/protocol/MakeMoveByBehavior.java
+++ src/main/java/castle/comp3021/assignment/protocol/MakeMoveByBehavior.java
@@ -2,6 +2,11 @@ package castle.comp3021.assignment.protocol;
 
 import castle.comp3021.assignment.piece.Knight;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class MakeMoveByBehavior {
     private final Behavior behavior;
     private final Game game;
@@ -27,7 +32,87 @@ public class MakeMoveByBehavior {
      */
     public Move getNextMove(){
         // TODO
-        return this.availableMoves[0];
+        if (availableMoves.length <= 0){
+            return null;
+        }
+        Move nextMove;
+        switch (behavior){
+            case GREEDY -> {
+                nextMove =
+                        Arrays.stream(availableMoves)
+                        .filter(move -> {
+                            if (move == null){
+                                return false;
+                            }
+                            return getCenterDistance(move.getDestination()) < getCenterDistance(move.getSource());
+                        })
+                        .min(Comparator.comparing(move -> getCenterDistance(move.getDestination())))
+                        .orElseGet(() -> availableMoves[new Random().nextInt(availableMoves.length)]);
+            }
+            case CAPTURING -> {
+                Move[] capturingMoves =
+                        Arrays.stream(availableMoves)
+                                .filter(move -> {
+                                    if (move == null){
+                                        return false;
+                                    }
+                                    Piece destPiece = game.getPiece(move.getDestination());
+
+                                    return ((destPiece != null)
+                                            && !(destPiece.getPlayer().equals(game.currentPlayer)));
+                                })
+                                .collect(Collectors.toList()).toArray(Move[]::new);
+
+                if (capturingMoves.length <= 0){
+                    nextMove = availableMoves[new Random().nextInt(availableMoves.length)];
+                }else{
+                    nextMove = capturingMoves[new Random().nextInt(capturingMoves.length)];
+                }
+            }
+            case BLOCKING -> {
+                Move[] blockingMoves =
+                        Arrays.stream(availableMoves)
+                                .filter(move -> {
+                                    if (move == null){
+                                        return false;
+                                    }
+                                    Place dest = move.getDestination();
+                                    int[] offsetX = {-1, 1, 0, 0};
+                                    int[] offsetY = {0, 0, -1, 1};
+                                    for (int i=0; i<offsetX.length; i++){
+                                        Piece adjPiece = game.getPiece(dest.x()+offsetX[i], dest.y()+offsetY[i]);
+                                        if (adjPiece !=null){
+                                            if (adjPiece instanceof Knight &&
+                                                    !(adjPiece.getPlayer().equals(game.currentPlayer))){
+                                                return true;
+                                            }
+                                        }
+                                    }
+                                    return false;
+                                })
+                                .collect(Collectors.toList()).toArray(Move[]::new);
+
+                if (blockingMoves.length <= 0){
+                    nextMove = availableMoves[new Random().nextInt(availableMoves.length)];
+                }else{
+                    nextMove = blockingMoves[new Random().nextInt(blockingMoves.length)];
+                }
+            }
+            default -> {
+                nextMove = availableMoves[new Random().nextInt(availableMoves.length)];
+            }
+        }
+        return nextMove;
+    }
+
+    private int getCenterDistance(Place place){
+        if (place == null){
+            return -1;
+        }
+        Place center = game.getCentralPlace();
+        int xChange = Math.abs(place.x() - center.x());
+        int yChange = Math.abs(place.y() - center.y());
+        return Math.abs(xChange + yChange);
     }
 }
 
